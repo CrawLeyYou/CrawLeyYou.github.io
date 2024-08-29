b9cc696f8dcb805e3e6127fe93b57ce7a45c9128
CrawLeyYou <60201017+CrawLeyYou@users.noreply.github.com>
2024-08-29 16:02:47 +0300

    Push Next + Shadcn base

---
+++ src/app/layout.jsx
@@ -0,0 +1,24 @@
+import { Inter as FontSans } from "next/font/google"
+import "./globals.css";
+
+import { cn } from "@/lib/utils"
+ 
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
+export const metadata = {
+  title: "CrawLeyYou"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body  className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}>{children}</body>
+    </html>
+  );
+}
